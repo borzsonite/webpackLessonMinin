f2364364b3eb9d1961ce631b6b86cb0db6773873
borzsonite <borzsonite@gmail.com>
2020-02-23 22:47:59 +0300

    time 1.27.27 all is working

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import WebpackLogo from './assets/webpack-logo'
 import xml from './assets/data.xml'
 import csv from '@/data.csv' // вместо пути ./assets можно укзать alias @ прописанный в webpackconfig.js
 import * as $ from 'jquery'; // импортируем все из библиотеку jquery в переменную $
-// $('pre').html(post.toString())
+const post = new Post("Hello Webpack", WebpackLogo);
+$('pre').html(post.toString())
 
-// const post = new Post("Hello Webpack", WebpackLogo);
 console.log('Post to string', post.toString());
-// console.log('JSON:', json)
-// console.log('xml:', xml)
-// console.log('csv:', csv)
+console.log('JSON:', json)
+console.log('xml:', xml)
+console.log('csv:', csv)

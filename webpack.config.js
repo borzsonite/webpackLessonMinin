49e029fea65e395e32453099d6f028d4f1226563
borzsonite <borzsonite@gmail.com>
2020-02-24 13:48:42 +0300

    copy-webpack-plugin added

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     context: path.resolve(__dirname, 'src'), // указывает путь в котором лежат исходные файлы для сборки
@@ -32,6 +33,12 @@ module.exports = {
             template: './index.html'
         }),
         new CleanWebpackPlugin(),
+        new CopyWebpackPlugin([
+            {
+                from: path.resolve(__dirname, 'src/favicon.ico'),
+                to: path.resolve(__dirname, 'dist')
+            }
+        ]),
     ],
     module: {
         rules: [
